4e0d91697437f275dc91adb390c09d0104f6deaa
Mitchel Alejo <michalejo94@gmail.com>
2023-11-25 14:50:40 -0600

    Feat:Created database and some routes. Added front end vite

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+//Will fix any issues with parsing
+app.use(express.json());
+
+//This is how we import our tables. Will automatically go through every table.
+const db = require("./models");
+
+//Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+  app.listen(3002, () => {
+    console.log("Server running on Port 3002");
+  });
+});
+//run anonymous function when the serving is running
---
+++ server/routes/Posts.js
@@ -0,0 +1,22 @@
+//contains all routes related to posts.
+
+//express has a routing system all ready implemented into the framework
+
+const express = require("express");
+const router = express.Router();
+const { Posts } = require("../models");
+
+router.get("/", async (req, res) => {
+  //   res.json("Hello World");
+  const postList = await Posts.findAll();
+  res.json(postList);
+});
+
+router.post("/", async (req, res) => {
+  //inserting the data into the database using sequelize
+  const post = req.body;
+  await Posts.create(post);
+  res.json(post);
+});
+
+module.exports = router;
